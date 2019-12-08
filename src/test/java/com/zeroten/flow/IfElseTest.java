8202512aeb902ee52ada489148064bbf73bca362
sunnydemon <905584921@qq.com>
2019-12-08 10:54:58 +0800

    first commit

---
+++ src/test/java/com/zeroten/flow/IfElseTest.java
@@ -0,0 +1,14 @@
+package com.zeroten.flow;
+
+import org.testng.annotations.Test;
+
+public class IfElseTest {
+
+    @Test
+    public void testIfElse() {
+        System.out.println("hello world!" +
+                "\nNever Give Up");
+
+    }
+
+}
