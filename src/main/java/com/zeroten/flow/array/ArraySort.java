849d666dd93d35351ccc64fdda578c26f32d6c26
sunnydemon <905584921@qq.com>
2019-12-15 23:46:05 +0800

    修改冒泡排序、插入排序

--- src/main/java/com/zeroten/flow/array/ArraySort.java
+++ src/main/java/com/zeroten/flow/array/ArraySort.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ArraySort {
 
+    //冒泡排序
     public static int[] sort(int[] arr){
         for (int index1 = 0; index1 < arr.length ;index1 ++){
             for(int index2 = index1 ;index2 <arr.length ; index2 ++){
@@ -16,6 +17,38 @@ public class ArraySort {
             }
         }
         return arr;
-
     }
+    //插入排序
+    public static int[] InsertionSort(int[] arr){
+        if(arr == null){
+            return null;
+        }
+        for (int index = 0; index < arr.length; index++){
+            for (int i = 0; i < arr.length - index - 1; i++) {
+                if (arr[i] > arr[i+1]) {
+                    arr[i] = arr[i+1] + arr[i];
+                    arr[i+1] = arr[i] - arr[i+1];
+                    arr[i] = arr[i] - arr[i+1];
+                }
+            }
+        }
+        return arr;
+    }
+    //选择排序
+    public static int[] SelectionSort(int[] arr ){
+        if (arr == null)
+            return null;
+        for (int index = 0; index < arr.length; index ++){
+            for (int i = index; i < arr.length; i++){
+                if (arr[index] < arr[i]) {
+                    arr[i] = arr[index] + arr[i];
+                    arr[index] = arr[i] - arr[index];
+                    arr[i] = arr[i] - arr[index];
+                }
+            }
+        }
+        return arr;
+    }
+
+
 }
--- src/test/java/com/zeroten/flow/array/ArraySortTest.java
+++ src/test/java/com/zeroten/flow/array/ArraySortTest.java
@@ -10,20 +10,27 @@ import static org.testng.Assert.*;
 
 public class ArraySortTest {
 
+
     @Test
     public void SortTest(){
         Random rand = new Random();
-        int[] arr =  new int[10];
+        int[] arr =  new int[10000];
         for (int i = 0; i < arr.length ;  i++){
-            arr[i] = rand.nextInt(100);
+            arr[i] = rand.nextInt(1000);
         }
         int[] arr1 = arr;
         System.out.println(Arrays.toString(arr1));
         Arrays.sort(arr);
+        long start = System.currentTimeMillis();
         Assert.assertEquals(ArraySort.sort(arr1),arr);
+
+        //Assert.assertEquals(ArraySort.InsertionSort(arr1),arr);
+        System.out.println(String.format("花费的时间 :%d ms",System.currentTimeMillis() -start));
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr));
     }
+
+
 
 
 }
