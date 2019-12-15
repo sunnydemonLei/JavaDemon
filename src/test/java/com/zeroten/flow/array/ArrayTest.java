92e61f5d49ddb18ffe9c35b808a53168de4c832a
sunnydemon <905584921@qq.com>
2019-12-15 16:54:40 +0800

    数组遍历

--- src/test/java/com/zeroten/flow/array/ArrayTest.java
+++ src/test/java/com/zeroten/flow/array/ArrayTest.java
@@ -1,4 +1,29 @@
 package com.zeroten.flow.array;
 
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
 public class ArrayTest {
+    //遍历数组的方法
+    @Test
+    public void TestArrays(){
+        Integer[] arr ={1,2,3,4,5};
+       /* for(int index = 0; index < arr.length; index++ ){
+            System.out.println("下标："+index+",存放的值是："+arr[index]);
+            System.out.println(String.format("下标：%d ,存放的值是：%d",index,arr[index]));
+        }*/
+
+       //for  each方法
+        /* int index = 0;
+         for(int val : arr){
+            System.out.println("第"+ index + "次取到" +val);
+            index ++;
+        }*/
+        //lambda方式
+        Arrays.asList(arr).forEach(val -> {
+            System.out.println("值是"+val);
+        });
+    }
+
 }
